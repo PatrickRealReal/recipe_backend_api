8c7972754ac78d57e4e7ea70ea99201070fe6a63
Patrick Xu <patrickxu@Patricks-MacBook-Pro-2.local>
2023-03-06 18:02:54 -0600

    Some updated documents

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,8 +7,6 @@ class CalcTests(SimpleTestCase):
     def test_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
     def test_Substract_numbers(self):
         res = calc.substract(5, 6)
         self.assertEqual(res, 1)
-        
